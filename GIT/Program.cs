fee437cce0883c7419173e9b76bffdbc1eac27d7
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-05-20 14:10:06 +0200

    Add Add functinon

--- GIT/Program.cs
+++ GIT/Program.cs
@@ -1,15 +1,22 @@
-﻿namespace GIT
+﻿using System.ComponentModel;
+
+namespace GIT
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-       
 
-            Console.WriteLine("Hej");
-            Console.WriteLine("Hej");
-            Console.WriteLine("Hej");
-            Console.WriteLine("Hej");
+            int x = 1;
+            int y = 2;
+
+            int result = Add(x, y);
+          
+        }
+
+        private static int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
