3db178cc8c322044aed8ea3a20d82bac1e8af64b
DZM77 <32109758+DZM77@users.noreply.github.com>
2024-09-10 14:01:07 +0200

    Remove unused code

--- GIT/Program.cs
+++ GIT/Program.cs
@@ -1,36 +1,21 @@
 ﻿using System.ComponentModel;
 
-namespace GIT
+namespace GIT;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
 
-            int x = 1;
-            int y = 2;
 
-            if (x != y)
-            {
-                //
-                int v = 3;
+        int result = Add(1, 2);
 
-            }
+      
+      
+    }
 
-            int result = Add(x, y);
-
-            int temp = 0;
-            for (int i = 0; i < 10000; i++)
-            {
-                //Other code
-                temp = i;
-            }
-          
-        }
-
-        private static int Add(int x, int y)
-        {
-            return x + y;
-        }
+    private static int Add(int x, int y)
+    {
+        return x + y;
     }
 }
